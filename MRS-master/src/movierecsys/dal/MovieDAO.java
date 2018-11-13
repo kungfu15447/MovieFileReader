8b0f63731185ab668216cc7fc2c1260e8c851776
Frederik Jensen <kungfu15447@users.noreply.github.com>
2018-11-13 11:12:42 +0100

    Lavede deleteMovie metode

--- MRS-master/src/movierecsys/dal/MovieDAO.java
+++ MRS-master/src/movierecsys/dal/MovieDAO.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.MalformedInputException;
@@ -93,7 +94,7 @@ public class MovieDAO
     public Movie createMovie(int releaseYear, String title) throws IOException
     {
         Path path = new File(MOVIE_SOURCE).toPath();
-        int id = -1;
+        int id;
         try (BufferedWriter bw = Files.newBufferedWriter(path, StandardOpenOption.SYNC, StandardOpenOption.APPEND, StandardOpenOption.WRITE))
         {
             id = getNextAvailableMovieID();
@@ -123,7 +124,31 @@ public class MovieDAO
      */
     private void deleteMovie(Movie movie)
     {
-        //TODO Delete movie
+        String tempFile = "temp.txt";
+        File oldFile = new File(MOVIE_SOURCE);
+        File newFile = new File(tempFile);
+        try
+        {
+            for (int i = 0; i < getAllMovies().size(); i++) {
+                if (movie.getId() == getAllMovies().get(i).getId()) {
+                    getAllMovies().remove(i);
+                }
+            }
+            FileWriter fw = new FileWriter(tempFile);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for(Movie move : getAllMovies()) {
+                bw.write(move.getId() + "," + move.getYear() + "," + move.getTitle());
+            }
+            bw.flush();
+            bw.close();
+            oldFile.delete();
+            File dump = new File(MOVIE_SOURCE);
+            newFile.renameTo(dump);
+            
+        }
+        catch(Exception e){
+            System.out.println("Something went wrong");
+        }
     }
 
     /**
