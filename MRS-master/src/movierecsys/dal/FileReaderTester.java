0d63763e768922a19882470701b2916235ca1810
Frederik Jensen <kungfu15447@users.noreply.github.com>
2018-11-14 13:44:38 +0100

    Lavede på RatingDao

--- MRS-master/src/movierecsys/dal/FileReaderTester.java
+++ MRS-master/src/movierecsys/dal/FileReaderTester.java
@@ -25,16 +25,7 @@ public class FileReaderTester
      */
     public static void main(String[] args) throws IOException
     {
-        MovieDAO movieDao = new MovieDAO();
-
-        Movie movie = movieDao.getMovie(1501); //Only run this once, or you will get multiple entries!
-        System.out.println(movie);
-        System.out.println("Hej");
-         //Only run this once, or you will get multiple entries!
-        Movie movie2 = new Movie(1, 2003, "Dinosaur Planet");
-        movieDao.updateMovie(movie2);
         
-        System.exit(0);
     }
     public void movieDAOMethods() throws IOException {
         MovieDAO movieDao = new MovieDAO();
